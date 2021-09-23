2222064c05bcb5b5b4cc63c6f1b2c7786c0dbc70
YuliChavdarov <yulianchavdarov@gmail.com>
2021-09-23 17:12:47 +0300

    Advanced-Querying exercise

--- Advanced-Querying/BookShop/StartUp.cs
+++ Advanced-Querying/BookShop/StartUp.cs
@@ -3,6 +3,7 @@
     using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore;
     using System;
     using System.Linq;
     using System.Text;
@@ -14,8 +15,7 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-            string input = Console.ReadLine();
-            Console.WriteLine(GetBooksNotReleasedIn(db, int.Parse(input)));
+            Console.WriteLine(GetBooksReleasedBefore(db, "12-04-1992"));
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -94,7 +94,51 @@
             return sb.ToString().TrimEnd();
         }
 
-        //https://www.youtube.com/watch?v=CRXMg7ibGIA
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            StringBuilder sb = new StringBuilder();
 
+            string[] categories = input.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var books = context.Books
+                .Include(x => x.BookCategories)
+                .ThenInclude(x => x.Category)
+                .ToArray()
+                .Where(x => x.BookCategories.Any(x => categories.Contains(x.Category.Name.ToLower())))
+                .Select(x => new { x.Title })
+                .OrderBy(x => x.Title)
+                .ToArray();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            DateTime releaseDate = DateTime.ParseExact(date, "dd-MM-yyyy", null);
+
+            var books = context.Books
+                .Where(x => x.ReleaseDate < releaseDate)
+                .OrderByDescending(x => x.ReleaseDate)
+                .Select(x => new 
+                { 
+                    x.Title, 
+                    x.EditionType, 
+                    x.Price 
+                })
+                .ToList();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - {book.EditionType} - ${book.Price:0.00}");
+            }
+            return sb.ToString().Trim();
+        }
     }
 }
