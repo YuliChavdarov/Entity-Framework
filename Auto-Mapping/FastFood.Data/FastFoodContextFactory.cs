50fd3bfbf5671528c607fa01776ef80d98f640c5
YuliChavdarov <yulianchavdarov@gmail.com>
2021-09-24 14:12:33 +0300

    Auto-Mapping exercise

---
+++ Auto-Mapping/FastFood.Data/FastFoodContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FastFoodContext>();
+            optionsBuilder.UseSqlServer("Server=.;Integrated security=true;Database=FastFood");
+
+            return new FastFoodContext(optionsBuilder.Options);
+        }
+    }
+}
