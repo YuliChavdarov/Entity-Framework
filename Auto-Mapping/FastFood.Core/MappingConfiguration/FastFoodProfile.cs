dfc6d52a2ac8fda4423f12c46cb04d7d2eda2b1c
YuliChavdarov <yulianchavdarov@gmail.com>
2021-09-24 14:46:04 +0300

    Auto-Mapping exercise

--- Auto-Mapping/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
+++ Auto-Mapping/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -7,6 +7,7 @@
     using FastFood.Core.ViewModels.Orders;
     using FastFood.Models;
     using System;
+    using System.Globalization;
     using ViewModels.Positions;
 
     public class FastFoodProfile : Profile
@@ -23,11 +24,13 @@
             this.CreateMap<Position, RegisterEmployeeViewModel>()
             .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
 
+            //Employees
             this.CreateMap<Employee, EmployeesAllViewModel>()
             .ForMember(x => x.Position, y => y.MapFrom(s => s.Position.Name));
 
             this.CreateMap<RegisterEmployeeInputModel, Employee>();
 
+            //Categories
             this.CreateMap<CreateCategoryInputModel, Category>()
                 .ForMember(x => x.Name, y => y.MapFrom(input => input.CategoryName));
 
@@ -36,17 +39,22 @@
             this.CreateMap<Category, CreateItemViewModel>()
                 .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
 
+            //Items
             this.CreateMap<CreateItemInputModel, Item>();
             
             this.CreateMap<Item, ItemsAllViewModels>()
                 .ForMember(x => x.Category, y => y.MapFrom(s => s.Category.Name));
 
+            //Orders
             this.CreateMap<CreateOrderInputModel, Order>()
                 .ForMember(x => x.DateTime, y => y.MapFrom(s => DateTime.Now));
+            
             this.CreateMap<CreateOrderInputModel, OrderItem>();
 
             this.CreateMap<Order, OrderAllViewModel>()
-                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name));
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name))
+                .ForMember(x => x.OrderId, y => y.MapFrom(s => s.Id))
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => s.DateTime.ToString("G", CultureInfo.InvariantCulture)));
         }
     }
 }
